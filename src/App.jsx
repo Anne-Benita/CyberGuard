9bcfa512d516e414bc76a54c0b3d3458656155f8
ANNE BENITA <benitaanne54@gmail.com>
2025-10-04 04:40:10 +0100

    Updated componentss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Sidebar from "./components/Sidebar";
 import React from "react";
 import Header from "./components/Header";
+
 function App() {
+
+  
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 
     dark:from-slate-900 dark:via-slate-800  dark:to-slate-900 transition-all duration-500">
